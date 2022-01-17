305651e852fcbeb5a7d6bbbc777b10e18442696d
Vadim <korobeinikovv@mail.ru>
2022-01-17 23:09:06 +0300

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from "react";
-import { Example as Example1 } from "./examples/1_data-provider";
-import { Example as Example2 } from "./examples/2_children";
-import { Example as Example3 } from "./examples/3_custom-component";
+import React from 'react'
+import { Example as Example1 } from './examples/1_data-provider'
+// import { Example as Example2 } from "./examples/2_children";
+// import { Example as Example3 } from "./examples/3_custom-component";
 
 function App() {
   return (
     <>
       <Example1 />
-      {/* <Example2 />
-      <Example3 /> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
